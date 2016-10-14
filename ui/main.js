640e7e2d6f37eb011dd7e7479ce98dead55725fe
AnnmarieAntony <annmarieantony@gmail.com>
2016-10-15 00:41:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,10 @@ console.log('Loaded!');
 //change main text
 var element = document.getElementById('main-text');
 element.innerHTML ='new value';
+
+//move image
+var img = document.getElementById('img');
+
+img.onclick = function(){
+   img.style.marginLeft = '100px';
+};
